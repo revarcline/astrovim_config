67f8a0156aba1e6b0cbe3093a822446522f1966d
Alex Cline <rev.a.r.cline@gmail.com>
2023-01-11 10:18:39 -0600

    more changes

--- init.lua
+++ init.lua
@@ -251,6 +251,13 @@ local config = {
     -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
     ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
       -- ensure_installed = { "prettier", "stylua" },
+        prettier = function()
+        require("null-ls").register(require("null-ls").builtins.formatting.prettier.with({
+          condition = function(utils)
+            return utils.root_has_file("package.json") or utils.root_has_file(".prettierrc") or utils.root_has_file(".prettierrc.json") or utils.root_has_file(".prettierrc.js")
+          end
+        }))
+      end,
     },
     ["mason-nvim-dap"] = { -- overrides `require("mason-nvim-dap").setup(...)`
       -- ensure_installed = { "python" },
